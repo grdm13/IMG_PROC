00f63d7fe6c0a21940d14ce5a4eaceebdcf84230
George <grdmgrdm@gmail.com>
2020-03-06 15:57:39 -0600

    - Started working on the actual script - Read the video - Count the frames

--- video.py
+++ video.py
@@ -2,4 +2,10 @@ import numpy as np
 import cv2
 
 # read the video
-Vid = cv2.VideoCapture('C:\\Users\\Dino\\PycharmProjects\\IMG_PROC\\mirror.mp4')
+video = cv2.VideoCapture('C:\\Users\\Dino\\PycharmProjects\\IMG_PROC\\mirror.mp4')
+
+# measure the video frames
+frames = 0
+frames = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+print(f'the total number of frames is:', frames)
+
