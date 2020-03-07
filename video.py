fd02ef189da00d196940c801b94fb1c096f0b337
George <grdmgrdm@gmail.com>
2020-03-07 12:16:33 -0600

    - reads video - prints frames, duration, fps - shows whole video

--- video.py
+++ video.py
@@ -6,22 +6,46 @@ video = cv2.VideoCapture('C:\\Users\\Dino\\PycharmProjects\\IMG_PROC\\mirror.mp4
 
 
 #show basic staff about the video
-frames = video.get(cv2.CAP_PROP_FRAME_COUNT)
-duration = video.get(cv2.cv2.CAP_PROP_POS_MSEC)
-fps = video.get(cv2.CAP_PROP_FPS)
-print(f'The total number of frames is:', frames, 'frames', ', the total duration of the video is:', duration,
+def BasicVideoStats():
+    frames = video.get(cv2.CAP_PROP_FRAME_COUNT)
+    duration = video.get(cv2.cv2.CAP_PROP_POS_MSEC)
+    fps = video.get(cv2.CAP_PROP_FPS)
+    print(f'The total number of frames is:', frames, 'frames', ', the total duration of the video is:', duration,
       'msec and the FPS of the video is:', fps)
 
 
 # show video --- to stop showing you have to press the letter q
-while (True):
-    ret, frame = video.read()
+def PlayVideo():
+    while (True):
+        ret, frame = video.read()
+        cv2.imshow('frame', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    video.release()
+    cv2.destroyAllWindows()
 
-    cv2.imshow('frame', frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+#Capture images per 40 frame
+def imagesplitter():
+    video_images = 'C:\\Users\\Dino\\PycharmProjects\\IMG_PROC\\VideoImages'
+    frameFrequency=40
+# iterate all frames
+    total_frame = 0
+    id = 0
+    while True:
+        ret, frame = video.read()
+        if ret is False:
+            break
+        total_frame += 1
+        if total_frame % frameFrequency == 0:
+            id += 1
+            image_name = video_images + str(id) + '.jpg'
+            cv2.imwrite(image_name, frame)
+            print(image_name)
+    video.release()
 
-video.release()
-cv2.destroyAllWindows()
+def FirstImage():
+    image = cv2.imread('C:\\Users\\Dino\\PycharmProjects\\IMG_PROC\\VideoImages1.jpg')
+    output = image.copy()
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-#reading and showing the first frame
+FirstImage()
