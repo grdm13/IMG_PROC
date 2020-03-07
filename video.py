ee5f7563c7214876d3fcadb596cefd979ebdfb9a
George <grdmgrdm@gmail.com>
2020-03-07 11:43:37 -0600

    - reads video - prints frames, duration, fps - shows whole video

--- video.py
+++ video.py
@@ -4,24 +4,24 @@ import cv2
 # read the video
 video = cv2.VideoCapture('C:\\Users\\Dino\\PycharmProjects\\IMG_PROC\\mirror.mp4')
 
-# measure the video frames
-frames = 0
-frames = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
-print(f'The total number of frames is:', frames, 'frames')
 
-# measure the duration of the video
-duration = 0
-#Checking shows that this sets the point after the last frame (not before it), so the timestamp is indeed the exact total length of the stream
-d = video.set(cv2.CAP_PROP_POS_AVI_RATIO,1)
-print(f'Are we at the last frame?', d)
-duration = int(video.get(cv2.CAP_PROP_POS_MSEC))
-duration_sec = duration / 1000
-print(f'The total duration of the video is:', duration_sec, 'sec')
+#show basic staff about the video
+frames = video.get(cv2.CAP_PROP_FRAME_COUNT)
+duration = video.get(cv2.cv2.CAP_PROP_POS_MSEC)
+fps = video.get(cv2.CAP_PROP_FPS)
+print(f'The total number of frames is:', frames, 'frames', ', the total duration of the video is:', duration,
+      'msec and the FPS of the video is:', fps)
 
-#finding out the fps of the video
-fps = 0
-fps = int(frames / duration_sec)
-print(f'The FPS of the video is:', fps)
+
+# show video --- to stop showing you have to press the letter q
+while (True):
+    ret, frame = video.read()
+
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+video.release()
+cv2.destroyAllWindows()
 
 #reading and showing the first frame
-
